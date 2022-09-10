63b11b30ed1fcf4638e52c4b9a09c0a54f1d5340
rizalkhi <rizal@samelement.com>
2022-09-10 14:42:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        navbar: "#D0D6DD",
+        side: "#626B79",
+        primary: "#232933",
+        warn: "#FBF3DD",
+        error: "#DB8000",
+        empty: "#FDECEE",
+      },
+    },
+  },
+  plugins: [],
+};
