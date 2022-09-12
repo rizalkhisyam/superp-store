fabb0f15e9ba4f41b2bf4a984802baebe259ca29
rizalkhisyam <162410101048@cs.unej.ac.id>
2022-09-12 13:41:47 +0700

    revisi UI monday

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        navbar: "#D0D6DD",
+        navbar: "#F2F4F7",
         side: "#626B79",
         primary: "#232933",
         warn: "#FBF3DD",
         error: "#DB8000",
         empty: "#FDECEE",
+        hover: "#F7601A",
+        icon: "#D0D6DD"
       },
     },
   },
